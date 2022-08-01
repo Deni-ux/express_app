c71de1b7a699401b16ffe79d05b0bd26479cceb9
Denisa Haka <denisa.haka@ymail.com>
2022-08-01 21:52:50 +0100

    create basic express app

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿const express = require("express");
+const app = express();
+const port = 3000;
+
+//code sets up a route to slash / with the syntax:
+app.get("/", (req, res) => res.send("hello world 🔥 ffddg!"));
+
+
+
+//the code starts the web application by invoking the listen() method
+app.listen(port, () =>
+  console.log(`Example express app listening on ${port}!`)
+);
